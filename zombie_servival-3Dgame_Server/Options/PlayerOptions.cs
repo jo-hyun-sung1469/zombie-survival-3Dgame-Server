587022c8f5ac9bae1c846f22608b0ab2b4c26fa5
Jo HyunSung <hunsung1469@gmail.com>
2026-06-19 16:04:43 +0900

    feat: 스텟 강화 단위 옵션 제작
    #17

--- zombie_servival-3Dgame_Server/Options/PlayerOptions.cs
+++ zombie_servival-3Dgame_Server/Options/PlayerOptions.cs
@@ -5,6 +5,7 @@ public sealed class PlayerOptions
     public const string SectionName = "Player";
 
     public PlayerBaseStatsOptions BaseStats { get; init; } = new();
+    public PlayerStatUpgradeOptions StatUpgrades { get; init; } = new();
 }
 
 public sealed class PlayerBaseStatsOptions
@@ -14,4 +15,18 @@ public sealed class PlayerBaseStatsOptions
     public int Defense { get; init; } = 5;
     public double MoveSpeed { get; init; } = 5.0;
     public double CriticalChance { get; init; } = 0.05;
+}
+
+public sealed class PlayerStatUpgradeOptions
+{
+    public int MaxLevel { get; init; } = 50;
+    public int BaseCost { get; init; } = 150;
+    public double CostIncreaseRate { get; init; } = 0.15;
+    public Dictionary<string, double> IncreasesByStat { get; init; } = new(StringComparer.OrdinalIgnoreCase)
+    {
+        ["MaxHealth"] = 50,
+        ["AttackPower"] = 5,
+        ["Defense"] = 1,
+        ["CriticalChance"] = 0.01
+    };
 }
