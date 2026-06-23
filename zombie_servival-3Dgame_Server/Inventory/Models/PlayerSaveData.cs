58a7618e8ffded26301b1e5a89264522a70565dd
Jo HyunSung <hunsung1469@gmail.com>
2026-06-23 19:45:25 +0900

    feat: PlayerStatUpgradeState를 저장하는 list 타입의 열 생성
    #17

--- zombie_servival-3Dgame_Server/Inventory/Models/PlayerSaveData.cs
+++ zombie_servival-3Dgame_Server/Inventory/Models/PlayerSaveData.cs
@@ -1,3 +1,5 @@
+using zombie_survival_3Dgame_Server.Player.Models;
+
 namespace zombie_survival_3Dgame_Server.Inventory.Models;
 
 public sealed class PlayerSaveData
@@ -7,4 +9,5 @@ public sealed class PlayerSaveData
     public int Gold { get; set; }
     public DateTime UpdatedAtUtc { get; set; }
     public List<PlayerWeaponState> WeaponStates { get; set; } = [];
+    public List<PlayerStatUpgradeState> StatUpgradeStates { get; set; } = [];
 }
