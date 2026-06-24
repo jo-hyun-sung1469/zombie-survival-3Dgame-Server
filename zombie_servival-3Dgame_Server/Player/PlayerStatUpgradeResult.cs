325611a0ae307de44b96e9757031a22db434246d
Jo HyunSung <hunsung1469@gmail.com>
2026-06-24 10:54:32 +0900

    feat: 서버 내부에서 강화 성공 여부를 반환하는 모델
    #17

---
+++ zombie_servival-3Dgame_Server/Player/PlayerStatUpgradeResult.cs
@@ -0,0 +1,16 @@
+using zombie_survival_3Dgame_Server.Contracts.Player;
+
+namespace zombie_survival_3Dgame_Server.Player;
+
+//강화 결과(처리 성공 여부)를 서버 내부에 반환하는 클래스(모델)
+public sealed class PlayerStatUpgradeResult
+{
+    public required PlayerStatUpgradeStatus Status { get; init; }
+    public int RequiredGold { get; init; }
+    public int CurrentGold { get; init; }
+    public int CurrentUpgradeLevel { get; init; }
+    public int MaxLevel { get; init; }
+    
+    //여기서 이름이나 강화량을 반환
+    public UpgradePlayerStatResponse? Response { get; init; }
+}
